167f1f228a9a4ab8dcd8722eafb5c98a9c9b7bb3
Qwerty <rosna-m@mail.ru>
2024-02-24 16:34:05 +0800

    refactoring

--- src/main/java/com/oshovsky/clean/architecture/image/storage/models/GetImageWrapper.java
+++
@@ -1,5 +0,0 @@
-package com.oshovsky.clean.architecture.image.storage.models;
-
-import org.springframework.core.io.Resource;
-
-public record GetImageWrapper(Resource resource, String filename, String mimeType) {}
--- src/main/java/com/oshovsky/clean/architecture/image/storage/models/SaveFileWrapper.java
+++
@@ -1,5 +0,0 @@
-package com.oshovsky.clean.architecture.image.storage.models;
-
-import java.util.UUID;
-
-public record SaveFileWrapper(UUID uuid) {}
